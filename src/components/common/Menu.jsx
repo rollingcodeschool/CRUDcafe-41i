d5a9a80d9c20a93f30e1abb0823ae75c65c39a94
Emilse Arias <emilsearias@gmail.com>
2023-06-05 17:25:28 -0300

    unimos las opciones del menu con las rutas

--- src/components/common/Menu.jsx
+++ src/components/common/Menu.jsx
@@ -1,18 +1,18 @@
 import { Navbar, Container, Nav } from "react-bootstrap";
-
+import {Link, NavLink} from 'react-router-dom';
 
 const Menu = () => {
   return (
     <Navbar bg="danger" variant="dark" expand="lg">
       <Container>
-        <Navbar.Brand href="#home">Rolling Café</Navbar.Brand>
+        <Navbar.Brand as={Link} to='/'>Rolling Café</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
-            <Nav.Link href="#">Inicio</Nav.Link>
-            <Nav.Link href="#">Registro</Nav.Link>
-            <Nav.Link href="#">Administrador</Nav.Link>
-            <Nav.Link href="#">Login</Nav.Link>
+            <NavLink end className={'nav-item nav-link'} to={'/'}>Inicio</NavLink>
+            <NavLink end className={'nav-item nav-link'} to={'/registro'}>Registro</NavLink>
+            <NavLink end className={'nav-item nav-link'} to={'/administrador'}>Administrador</NavLink>
+            <NavLink end className={'nav-item nav-link'} to={'/login'}>Login</NavLink>
           </Nav>
         </Navbar.Collapse>
       </Container>
