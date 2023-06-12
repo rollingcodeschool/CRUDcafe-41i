b3bd6317e2e49174f0bd19d13373ea710ec13ec6
Emilse Arias <emilsearias@gmail.com>
2023-06-12 15:07:55 -0300

    agregue la tarea de validaciones del formulario crear y editar producto

--- src/components/views/producto/EditarProducto.jsx
+++ src/components/views/producto/EditarProducto.jsx
@@ -1,40 +1,85 @@
 import { Form, Button } from "react-bootstrap";
+import { useForm } from "react-hook-form";
 
 const EditarProducto = () => {
-  
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm();
+
+  const onSubmit = (productoNuevo) => {
+    console.log(productoNuevo);
+   
+  };
+
   return (
     <section className="container mainSection">
       <h1 className="display-4 mt-5">Editar producto</h1>
       <hr />
-      <Form >
+      <Form onSubmit={handleSubmit(onSubmit)}>
         <Form.Group className="mb-3" controlId="formNombreProdcuto">
           <Form.Label>Producto*</Form.Label>
           <Form.Control
             type="text"
             placeholder="Ej: Cafe"
+            {...register("nombreProducto", {
+              required: "El nombre del producto es obligatorio",
+              minLength: {
+                value: 2,
+                message: "La cantidad minima de caracteres es de 2 digitos",
+              },
+              maxLength: {
+                value: 100,
+                message: "La cantidad minima de caracteres es de 2 digitos",
+              },
+            })}
           />
-         
+          <Form.Text className="text-danger">
+            {errors.nombreProducto?.message}
+          </Form.Text>
         </Form.Group>
         <Form.Group className="mb-3" controlId="formPrecio">
           <Form.Label>Precio*</Form.Label>
           <Form.Control
             type="number"
             placeholder="Ej: 50"
+            {...register("precio", {
+              required: "El precio del producto es obligatorio",
+              min: {
+                value: 1,
+                message: "El precio minimo es de $1",
+              },
+              max: {
+                value: 10000,
+                message: "El precio maximo es de $10000",
+              },
+            })}
           />
-       
+          <Form.Text className="text-danger">
+            {errors.precio?.message}
+          </Form.Text>
         </Form.Group>
         <Form.Group className="mb-3" controlId="formImagen">
           <Form.Label>Imagen URL*</Form.Label>
           <Form.Control
             type="text"
             placeholder="Ej: https://www.pexels.com/es-es/vans-en-blanco-y-negro-fuera-de-la-decoracion-para-colgar-en-la-pared-1230679/"
-      
+            {...register("imagen", {
+              required: "La imagen es obligatoria",
+            })}
           />
-      
+          <Form.Text className="text-danger">
+            {errors.imagen?.message}
+          </Form.Text>
         </Form.Group>
         <Form.Group className="mb-3" controlId="formPrecio">
           <Form.Label>Categoria*</Form.Label>
           <Form.Select
+            {...register("categoria", {
+              required: "La imagen es obligatoria",
+            })}
           >
             <option value="">Seleccione una opcion</option>
             <option value="bebida caliente">Bebida caliente</option>
@@ -42,7 +87,10 @@ const EditarProducto = () => {
             <option value="dulce">Dulce</option>
             <option value="salado">Salado</option>
           </Form.Select>
-       
+          <Form.Text className="text-danger">
+            {errors.categoria?.message}
+            
+          </Form.Text>
         </Form.Group>
         <Button variant="primary" type="submit">
           Guardar
